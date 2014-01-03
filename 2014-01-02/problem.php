e1c5a54d85317f6115450f7e76f696d28e8ffe6b
chadicus <chadwickgray@gmail.com>
2014-01-03 10:59:41 -0500

    Cleanup trailing whitespace

--- 2014-01-02/problem.php
+++ 2014-01-02/problem.php
@@ -1,15 +1,15 @@
-<?php                                                                                                                                            
-/**                                                                                                                                              
- * Returns the number of business hours between two dates.                                                                                       
- *                                                                                                                                               
- * @param \DateTime $start The start date.                                                                                                       
- * @param \DateTime $end   The end date.                                                                                                         
- *                                                                                                                                               
- * @return float The number of business hours between the two dates                                                                              
- */                                                                                                                                              
-function getBusinessHours(\DateTime $start, \DateTime $end)                                                                                      
-{                                                                                                                                                
-    /** Business Hours are from 9 - 5 Mon - Fri **/                                                                                              
-}                                                                                                                                                
-                                                                                                                                                 
+<?php
+/**
+ * Returns the number of business hours between two dates.
+ *
+ * @param \DateTime $start The start date.
+ * @param \DateTime $end   The end date.
+ *
+ * @return float The number of business hours between the two dates
+ */
+function getBusinessHours(\DateTime $start, \DateTime $end)
+{
+    /** Business Hours are from 9 - 5 Mon - Fri **/
+}
+
 assert(getBusinessHours(new \DateTime('01/02/2014 13:00:00'), new DateTime('01/03/2014 12:00:00')) === 8.0);
