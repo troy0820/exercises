6e2e989d98516f75262367f3631b8e176d0aa7a8
chadicus <chadwickgray@gmail.com>
2014-01-03 11:00:01 -0500

    Add more assert calls

--- 2014-01-02/problem.php
+++ 2014-01-02/problem.php
@@ -13,3 +13,5 @@ function getBusinessHours(\DateTime $start, \DateTime $end)
 }
 
 assert(getBusinessHours(new \DateTime('01/02/2014 13:00:00'), new DateTime('01/03/2014 12:00:00')) === 8.0);
+assert(getBusinessHours(new \DateTime('01/03/2014 17:00:00'), new DateTime('01/06/2014 09:00:00')) === 0.0);
+assert(getBusinessHours(new \DateTime('01/02/2014 09:00:00'), new DateTime('01/02/2014 12:00:00')) === 3.0);
